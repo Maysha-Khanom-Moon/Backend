69da27e51c552038232e6dbfe646196697a72099
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-09-25 23:31:43 +0600

    .env for production level security

--- chai-backend-1/index.js
+++ chai-backend-1/index.js
@@ -1,11 +1,29 @@
-console.log("Hello from chai-backend-1");
-
+require('dotenv').config();
 const express = require('express');
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
+
+const userData = {
+    name: 'moondotcom',
+    age: 24,
+    email: 'moon@gmail.com',
+    city: 'rajshahi',
+    college: 'RUET',
+    profession: 'student',
+    address: '216/1 Kajla, Rajshahi, Bangladesh'
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/twitter', (req, res) => {
+  res.json('moondotcom');
+});
+
+app.get('/user', (req, res) => {
+  res.json(userData);
 });
 
 app.listen(port, () => {
