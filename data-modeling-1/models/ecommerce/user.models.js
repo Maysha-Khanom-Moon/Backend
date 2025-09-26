21bf76df8a67b706e3cbbbf5bff2d143df5331f2
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-09-26 22:53:48 +0600

    ecommerce db: user, category, product models

---
+++ data-modeling-1/models/ecommerce/user.models.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        minLength: 3,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    }
+}, {timestamps: true})
+
+export const User = mongoose.model("User", userSchema)
